5400d4c9b26b888a33093e92fb0624d7e866fd68
now <ryansnow@foxmial.com>
2018-12-31 16:20:01 +0800

    数据库表更新

--- src/main/java/com/springboot/modules/sys/service/impl/UserServiceImpl.java
+++ src/main/java/com/springboot/modules/sys/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.springboot.modules.sys.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.EmptyWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.springboot.modules.sys.entity.UserEntity;
--- src/main/resources/springboot_demo.sql
+++ src/main/resources/springboot_demo.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 50642
  Source Host           : localhost:3306
@@ -11,11 +11,27 @@
  Target Server Version : 50642
  File Encoding         : 65001
 
- Date: 24/12/2018 16:30:58
+ Date: 31/12/2018 16:17:47
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `operation` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户操作',
+  `method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求方法',
+  `params` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求参数',
+  `time` mediumint(9) NULL DEFAULT NULL COMMENT '执行时长',
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'ip地址',
+  `create_date` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for sys_permission
@@ -31,11 +47,11 @@ CREATE TABLE `sys_permission`  (
 -- ----------------------------
 -- Records of sys_permission
 -- ----------------------------
-INSERT INTO `sys_permission` VALUES (1, 'sys:role:add', 1);
+INSERT INTO `sys_permission` VALUES (1, 'sys:role:save', 1);
 INSERT INTO `sys_permission` VALUES (2, 'sys:role:update', 1);
 INSERT INTO `sys_permission` VALUES (3, 'sys:role:select', 1);
 INSERT INTO `sys_permission` VALUES (4, 'sys:role:delete', 1);
-INSERT INTO `sys_permission` VALUES (5, 'sys:user:add', 1);
+INSERT INTO `sys_permission` VALUES (5, 'sys:user:save', 1);
 INSERT INTO `sys_permission` VALUES (6, 'sys:user:update', 1);
 INSERT INTO `sys_permission` VALUES (7, 'sys:user:select', 1);
 INSERT INTO `sys_permission` VALUES (8, 'sys:user:delete', 1);
@@ -49,7 +65,7 @@ CREATE TABLE `sys_role`  (
   `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `create_user_id` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_role
@@ -65,7 +81,7 @@ CREATE TABLE `sys_role_permission`  (
   `role_id` bigint(20) NOT NULL,
   `permission_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_role_permission
@@ -78,6 +94,8 @@ INSERT INTO `sys_role_permission` VALUES (5, 1, 5);
 INSERT INTO `sys_role_permission` VALUES (6, 1, 6);
 INSERT INTO `sys_role_permission` VALUES (7, 1, 7);
 INSERT INTO `sys_role_permission` VALUES (8, 1, 8);
+INSERT INTO `sys_role_permission` VALUES (9, 4, 1);
+INSERT INTO `sys_role_permission` VALUES (10, 4, 2);
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -97,7 +115,7 @@ CREATE TABLE `sys_user`  (
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES (1, 'admin', '123456', NULL, 1, 1);
-INSERT INTO `sys_user` VALUES (2, 'tester', '123456', NULL, NULL, NULL);
+INSERT INTO `sys_user` VALUES (2, 'tester', '123456', NULL, 1, 1);
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -108,11 +126,12 @@ CREATE TABLE `sys_user_role`  (
   `user_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
-INSERT INTO `sys_user_role` VALUES (1, 2, 1);
+INSERT INTO `sys_user_role` VALUES (1, 1, 1);
+INSERT INTO `sys_user_role` VALUES (2, 2, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
